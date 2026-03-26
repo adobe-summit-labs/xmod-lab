99e5ed1b511db7a5706abd6e713ad212c89b7f48
Gabriel Walt <gwalt@adobe.com>
2026-03-26 14:50:42 +0100

    updated blocks

---
+++ blocks/how-we-work/how-we-work.js
@@ -0,0 +1,5 @@
+export default async function decorate(block) {
+  // The block structure is already correct:
+  // Each row has two cells: [number, text content]
+  // No additional DOM restructuring needed.
+}
